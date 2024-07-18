e35504eceb09d9523c1efcb74cef09f32c0e7e8f
stefanzan <stefanzan@163.com>
2024-07-18 22:07:50 +0800

    [refactor] of example

--- test/bx/biEval.test.ts
+++ test/bx/biEval.test.ts
@@ -3,19 +3,11 @@ import * as SimpleCoreExample from "../core/0.simple.test";
 import * as CorePrint from "../../src/core/Print";
 import * as CorePrettyPrint from "../../src/core/PrettyPrint";
 
-console.log("========= Forward Evaluation ========");
-
 console.log("-------- 1. Assignment ----------");
+console.log("== Forward Evaluation ==");
 console.log(BiEval.forward(SimpleCoreExample.assignmentExample));
-
-console.log("-------- 2. Branch ----------");
-console.log(BiEval.forward(SimpleCoreExample.branchExample));
-
-console.log("-------- 3. Loop ----------");
-console.log(BiEval.forward(SimpleCoreExample.loopExample));
-
-
-console.log("========= Backward Evaluation ========");
+console.log("== Backward Evaluation ==");
+console.log('Operation:\n replace "1" with "2" at 3');
 console.log("Original:");
 // CorePrint.printAST(SimpleCoreExample.assignmentExample);
 console.log(CorePrettyPrint.prettyPrint(SimpleCoreExample.assignmentExample));
@@ -26,3 +18,9 @@ BiEval.backward(SimpleCoreExample.assignmentExample, {type:'replace', str1:"1", 
   console.log(CorePrettyPrint.prettyPrint(updatedCoreAST));
   console.log("~~~~~~~~~~~~~~~~~~~~~~~~");
 });
+console.log("-------- 2. Branch ----------");
+console.log(BiEval.forward(SimpleCoreExample.branchExample));
+
+console.log("-------- 3. Loop ----------");
+console.log(BiEval.forward(SimpleCoreExample.loopExample));
+
